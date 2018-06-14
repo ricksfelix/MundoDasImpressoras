f1034df30c02c20554344936a41f990cba675ba1
ricksfelix <ricksfelix@gmail.com>
2018-06-14 19:12:45 -0300

    Em Desenvolvimento

--- Func/bcmanage.py
+++ Func/bcmanage.py
@@ -1,5 +1,18 @@
 import sqlite3
 con = sqlite3.connect("database.db")
 c = con.cursor()
-
-#def login(user, passw):
+#
+#             EM DESENVOLVIMENTO
+#
+class DatabaseManager():
+    def create():
+        c.execute("CREATE TABLE IF NOT EXISTS Users(id interger, user text, passw text, cargo text)")
+        c.execute("CREATE TABLE IF NOT EXISTS Clientes(id interger, name text, cell text, tell text, addres text, "
+                  "email text)")
+        #c.execute("CREATE TABLE IF NOT EXISTS Equipamentos")
+    def read():
+        pass
+    def update():
+        pass
+    def delete():
+        pass
